6d8e5d8ee5f8e5a62aa90c16706984e4fb830aca
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2021-10-06 00:26:52 +0530

    Sum Of Digit Of The given number

--- SumOfDigitOfGivenNumber.cpp
+++ SumOfDigitOfGivenNumber.cpp
@@ -1,9 +1,7 @@
 // Program To Print The Sum Of Digit Of The given number
-
 #include<iostream>
 #include<string.h>
 using namespace std;
-
 int main()
 {
     char n[20];
@@ -16,7 +14,6 @@ int main()
     {
         sum += n[i] - '0';    // This line converts and seperate the string to number
     }
-    
     cout << sum;
     return 0;
 }
